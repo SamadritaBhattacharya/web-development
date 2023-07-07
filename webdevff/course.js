dfbb5053bdd8541f7d18f1a19dd13a9b8c5928c4
Samadrita Bhattacharya <136957481+SamadritaBhattacharya@users.noreply.github.com>
2023-07-07 15:25:18 +0700

    Revert "course uploaded "

--- webdevff/course.js
+++ webdevff/course.js
@@ -6,25 +6,9 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-// Middleware 
-const adminAuthentication = (req,res,next)=>{
-    const {username,password }= req.headers;
-    // the above code is equal to 
-    // const username= req.headers.username;
-    //const password = req.headers.password;
-    const admin = ADMINS.find( a=> a.username === username && a.password === password);
-    // checks if an admin or not 
-    if(admin){
-        next();  // calls the next callback function from where it is called previously, ie, the login()
-    }
-    else{
-        res.status(403).json({ message: 'Admin authentication failed.'});
-    }        
-};
-
 // sign up for admins for admins router 
 app.post('/admin/signup',(req,res)=>{
-    const admin = req.body;  //admins ={username:"abc123@gmail.com", password: "amkan123213"}
+    const admin = req.body;  //admins username , email and password created.
     const existingAdmin = ADMINS.find(a=> a.username === admin.username);
     // if he/she is an existing user 
     if(existingAdmin){
@@ -35,15 +19,3 @@ app.post('/admin/signup',(req,res)=>{
         res.json({ message:'Admin created successfully' });
     }
 });
-
-// admin login 
-app.post('/admin/login',adminAuthentication,(req,res)=>{
-    res.json({ message:'Logged in successfully'});
-});
-// courses
-app.post('admin/courses', adminAuthentication, (req,res)=>{
-    const course = req.body;
-    course.id = Date.now(); // use timestamp as course ID
-    COURSES.push(course);
-    res.json({ message: 'Course updated successfully', courseId: course.id});
-})
