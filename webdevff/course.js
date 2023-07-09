fd4e5d8e74bd472e4a1705a7d021378c350f22ee
Samadrita Bhattacharya <136957481+SamadritaBhattacharya@users.noreply.github.com>
2023-07-09 13:46:54 +0700

    course updated

--- webdevff/course.js
+++ webdevff/course.js
@@ -34,7 +34,7 @@ const userAuthentication = (req,res,next)=>{
         res.status(403).json({ message: 'User authentication failed.'});
     }
 };
- 
+
 // sign up for admins for admins router 
 app.post('/admin/signup',(req,res)=>{
     const admin = req.body;  //admins ={username:"abc123@gmail.com", password: "amkan123213"}
@@ -50,13 +50,98 @@ app.post('/admin/signup',(req,res)=>{
 });
 
 // admin login 
+// calling middleware 
 app.post('/admin/login',adminAuthentication,(req,res)=>{
     res.json({ message:'Logged in successfully'});
 });
 // courses
 app.post('admin/courses', adminAuthentication, (req,res)=>{
     const course = req.body;
+
+    // if(!course.title){
+    //     return res.json(411).send({ message:'Please send me the title '})
+    // }   check for proper title 
     course.id = Date.now(); // use timestamp as course ID
     COURSES.push(course);
     res.json({ message: 'Course updated successfully', courseId: course.id});
 })
+// updating courses from admin 
+app.put('admin/courses/:courseId',adminAuthentication, (req,res)=>{
+    const courseId = parseInt(req.params.courseId);
+    const course = COURSES.find(c=> c.id=== courseId); // the random id returned in the post courses function in line 64
+    if(course){
+        Object.assign(course,req.body); // updated in-memory replace  
+        // can also be written as 
+        // course.title = req.body.title;
+        // course.price = req.body.price;
+        res.json({ message: 'Course updated successfully '});
+    }
+    else{
+        res.status(404).json({ message: 'Course not found '});
+    }
+});
+
+app.get('/admin/courses', adminAuthentication, (req, res) => {
+    res.json({ courses: COURSES });
+  });
+
+  // user signup 
+  app.post('/users/signup', userAuthentication, (req,res)=>{
+    const user = {...req.body , purchasedCourses:[]};
+    // // the above code is equal to 
+    // // const user ={
+    //     username: req.body.username'
+    //     password: req.body.password '
+    //     purchasedCourses:[]
+    // }
+    USERS.push(user);
+    res.json({ message: 'User created successfully '});
+  });
+
+  // users login 
+  app.post('/users/login', userAuthentication,(req,res)=>{
+    res.json({ message: 'Logged in successfully '});
+  });
+// getting courses from users  
+  app.get('/users/courses', userAuthentication,(req,res)=>{
+    res.json({ courses: COURSES.filter(c => c.published)});
+    // let filteredCourses=[];
+    // for(let i=0;i<COURSES.length;i++){
+    //     if(COURSES[i].published){
+    //         filteredCourses.push(COURSES[i]);
+    //     }
+    // res.json({ courses: filteredCourses});
+    // }  same logic for above .filter() one 
+  });
+
+  // users purchase the course
+  app.post('/users/courses/:courseId', userAuthentication, (req, res) => {
+    const courseId = Number(req.params.courseId);
+    const course = COURSES.find(c => c.id === courseId && c.published);
+    if (course) {
+      req.user.purchasedCourses.push(courseId);
+      res.json({ message: 'Course purchased successfully' });
+    } else {
+      res.status(404).json({ message: 'Course not found or not available' });
+    }
+  });
+
+  // purchased courses 
+  app.get('/users/purchasedCourses', userAuthentication, (req, res) => {
+    // const purchasedCourses = COURSES.filter(c => req.user.purchasedCourses.includes(c.id));
+    // We need to extract the complete course object from COURSES
+    // which have ids which are present in req.user.purchasedCourses
+    var purchasedCourseIds = req.user.purchasedCourses; [1, 4];
+    var purchasedCourses = [];
+    for (let i = 0; i<COURSES.length; i++) {
+      if (purchasedCourseIds.indexOf(COURSES[i].id) !== -1) {
+        purchasedCourses.push(COURSES[i]);
+      }
+    }
+  
+    res.json({ purchasedCourses });
+  });
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
